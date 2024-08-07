0edc21ddcea07ed1eddb9438a23fff853376e0c1
rabruhin <167390949+rabruhin@users.noreply.github.com>
2024-08-07 23:18:29 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,6 +11,11 @@ app = Flask(__name__)
 def scrape_data():
     url = 'https://www.ntsh.ntpc.edu.tw/'
     
+    # 打印工作目录，检查ChromeDriver路径
+    print("Current Working Directory:", os.getcwd())
+    chrome_driver_path = os.path.abspath('./bin/chromedriver')
+    print("ChromeDriver Path:", chrome_driver_path)
+    
     # 配置ChromeDriver
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')  # 以无头模式运行
@@ -18,7 +23,7 @@ def scrape_data():
     options.add_argument('--disable-dev-shm-usage')
 
     # 指定ChromeDriver的路径
-    chrome_service = ChromeService(executable_path='./bin/chromedriver')
+    chrome_service = ChromeService(executable_path=chrome_driver_path)
 
     driver = webdriver.Chrome(service=chrome_service, options=options)
     driver.get(url)
@@ -53,4 +58,5 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
