4bba26b6c64d08e0bf2e9a97b6439941f290d120
rabruhin <167390949+rabruhin@users.noreply.github.com>
2024-08-07 23:57:56 +0800

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -3,19 +3,21 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Create directories for Chrome and ChromeDriver
+mkdir -p /opt/render/project/src/chrome
+mkdir -p /opt/render/project/src/chromedriver
+
 # Install Chrome
 echo "Installing Chrome..."
 wget -q -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-mkdir -p /tmp/chrome
-dpkg-deb -x google-chrome-stable_current_amd64.deb /tmp/chrome
+dpkg-deb -x google-chrome-stable_current_amd64.deb /opt/render/project/src/chrome
 rm google-chrome-stable_current_amd64.deb
-mv /tmp/chrome/opt/google/chrome /opt/chrome
 
 # Install ChromeDriver
 echo "Installing ChromeDriver..."
 CHROMEDRIVER_VERSION=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget -q -O chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /opt/chrome/
+unzip chromedriver_linux64.zip -d /opt/render/project/src/chromedriver
 rm chromedriver_linux64.zip
 
 echo "Installation completed."
