1889a9f47cd258aba6542405dd0c14609e99905d
rabruhin <167390949+rabruhin@users.noreply.github.com>
2024-08-08 15:45:52 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -50,6 +50,10 @@ def index():
     data = scrape_data()
     return render_template('index.html', data=data)
 
+@app.route("/",methods=["GET"])
+def home():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
