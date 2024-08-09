a2b43f2406946405039e305cee443dc2dfdcf601
rabruhin <167390949+rabruhin@users.noreply.github.com>
2024-08-09 17:38:09 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from flask import Flask, render_template
+from selenium.webdriver.chrome.service import Service
+import os
+
+
 def set_up_driver(chromedriver_path):
     options = Options()
     options.headless = False
